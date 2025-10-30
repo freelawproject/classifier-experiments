03d247abfe0b122300699f5346d02ab485702742
Nathan Dahlberg <nadahlberg@gmail.com>
2025-10-30 11:52:06 -0400

    feat(clx): add utils for downloading and extracting zip

--- clx/__init__.py
+++ clx/__init__.py
@@ -1,9 +1,15 @@
 from dotenv import load_dotenv
 
 from .utils import (
+    download_file,
+    extract_bz2_file,
     init_django,
 )
 
 load_dotenv()
 
-__all__ = ["init_django"]
+__all__ = [
+    "download_file",
+    "extract_bz2_file",
+    "init_django",
+]
