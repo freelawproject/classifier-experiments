37ebf158e3553bf2ba026d8576aaf2f278a14b17
Nathan Dahlberg <nadahlberg@gmail.com>
2025-12-10 13:33:55 -0500

    feat(cli) update cache-datasets command to use project data dir

--- clx/cli/cache_datasets.py
+++ clx/cli/cache_datasets.py
@@ -2,7 +2,6 @@ import click
 from tqdm import tqdm
 
 from clx import pd_save_or_append
-from clx.settings import CACHED_DATASET_DIR
 
 
 @click.command()
@@ -10,12 +9,13 @@ def cache_datasets():
     """Dump search datasets to CSV files."""
     from clx.models import Project
 
-    CACHED_DATASET_DIR.mkdir(parents=True, exist_ok=True)
-
     for project in tqdm(Project.objects.all(), desc="Caching datasets"):
         model = project.get_search_model()
-        dataset_path = CACHED_DATASET_DIR / f"{project.id}.csv"
+        dataset_path = project.cached_documents_path
+        dataset_path.parent.mkdir(parents=True, exist_ok=True)
         if dataset_path.exists():
             dataset_path.unlink()
-        for data in model.objects.batch_df("id", "text", batch_size=500000):
+        for data in model.objects.batch_df(
+            "id", "text_hash", "text", batch_size=500000
+        ):
             pd_save_or_append(data, dataset_path)
